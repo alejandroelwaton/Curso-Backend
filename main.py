8ed977ceea91abd12141e6482d9e4595cbee205a
Alejandro Sanchez <alej030611@gmail,.com>
2022-08-01 15:22:40 -0500

    response_request_body

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-import fastapi
+#Python
+from typing import Optional
 
-app = fastapi.FastAPI()
+#Pydantic
+from pydantic import BaseModel
+
+#Fast api
+from fastapi import FastAPI
+from fastapi import Body
+
+app = FastAPI()
+
+#Models
+
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str
+    time_in_stock: int
+    in_existence: Optional[bool] = False
 
 whatever = 'Random Value'
 
 @app.get('/')
 def home():
     return {'message': whatever}
+
+
+@app.post('/item/new')
+def new_item(item: Item = Body(...)):
+    return item
